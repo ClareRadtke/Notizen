f11d57222bec009474c2dcd629cc606433fff678
Clare Radtke <clare.radtke@outlook.com>
2021-05-12 21:29:01 +1000

    Set up note api endpoints

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
+const db = require("../db/db.json");
 
+app.use(express.static("public"));
+app.use(express.json());
 // TODO: input * for home/index.html route
 // app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../index.html"));
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
 // });
-// app.get("/notes.html", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../notes.html"));
-// });
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.send(JSON.stringify(db));
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  db.push(newNote);
+  res.json(newNote);
+});
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
